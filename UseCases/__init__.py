e8bb9075645ad3bcfd3777643a4754da6dc5a786
Arno-Can Uestuensoez <acue_sf2@sourceforge.net>
2019-12-05 04:21:26 +0100

    FisLegacy

--- UseCases/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'Arno-Can Uestuensoez'
-__license__ = "Artistic-License-2.0 + Forced-Fairplay-Constraints"
-__copyright__ = "Copyright (C) 2015-2016 Arno-Can Uestuensoez @Ingenieurbuero Arno-Can Uestuensoez"
-__version__ = '0.1.2'
-__uuid__ = 'efed42d3-f801-4fbb-abfd-bd598d683a82'
--- UseCases/objectinfo/PySourceInfo_check.py
+++
@@ -1,4 +0,0 @@
-#from __future__ import absolute_import
-
-def check_callback(fct,*args,**kargs):
-    return fct(*args,**kargs)
